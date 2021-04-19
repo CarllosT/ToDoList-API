340d77facf6eb8108b9d62b3ae4fdafa999b0bf8
Larissa Silva <vinitrajano@hotmail.com>
2021-04-19 20:55:21 -0300

    atualizando

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const chalk = require('chalk');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-const port = 3000
-
+const cors = require('cors')
+const bd = require('./infra/sqlite-db');
+const app = express();
+const port = 3000;
 const usuarioController = require('./Controllers/usuario-controller')
 const tarefaController = require('./Controllers/tarefa-controller')
 
-app.use(bodyParser.json())
-usuarioController(app);
-tarefaController(app);
+app.use(bodyParser.json());
+app.use(cors())
+
+usuarioController(app, bd);
+tarefaController(app, bd);
 
 app.listen(port, () => {
     console.log(chalk.green('[SUCESSO]: '),`Example app listening at http://localhost:${port}`)
